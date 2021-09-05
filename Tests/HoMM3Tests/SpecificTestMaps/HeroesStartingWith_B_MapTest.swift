817fb8b17146f72cd9d338b42f434f9b699c1d69
Alexander Cyon <alex.cyon@gmail.com>
2021-09-05 14:17:07 +0200

    Yet another test of available heroes

--- Tests/HoMM3Tests/SpecificTestMaps/HeroesStartingWith_B_MapTest.swift
+++ Tests/HoMM3Tests/SpecificTestMaps/HeroesStartingWith_B_MapTest.swift
@@ -1,6 +1,6 @@
 //
-//  HeroesStartingWith_A_MapTest.swift
-//  HeroesStartingWith_A_MapTest
+//  HeroesStartingWith_B_MapTest.swift
+//  HeroesStartingWith_B_MapTest
 //
 //  Created by Alexander Cyon on 2021-09-05.
 //
@@ -9,10 +9,10 @@ import Foundation
 import XCTest
 @testable import HoMM3SwiftUI
 
-final class HeroesStartingWith_A_Tests: AdditionalInfoBaseTests {
+final class HeroesStartingWith_B_Tests: AdditionalInfoBaseTests {
     
-    override var mapFileName: String { "cyon_sod_additional_info_heroes_starting_with_letter_A" }
-    override var mapName: String { "additional_info_heroes_on_A" }
+    override var mapFileName: String { "cyon_sod_additional_info_heroes_starting_with_letter_B" }
+    override var mapName: String { "additional_info_heroes_on_B" }
     
     func testHeroesStartingWithLetter_A() throws {
         let expectationHeroes = expectation(description: "Heroes")
@@ -22,7 +22,7 @@ final class HeroesStartingWith_A_Tests: AdditionalInfoBaseTests {
             onParseAvailableHeroes: { availableHeroes in
                 defer { expectationHeroes.fulfill() }
                 /// Sorting is the one of Map Editor: According to faction.
-                let expected = Set<Hero.ID>([.adela, .adelaide, .alagar, .aeris, .astral, .aine, .ayden, .axsis, .ash, .aislinn, .arlach,. ajit, .alamar, .alkin, .andra, .adrienne, .aenain])
+                let expected = Set<Hero.ID>([.boragus, .bron, .broghild, .brissa])
                 XCTAssertEqual(Set(availableHeroes.heroIDs), expected)
             }
         )
